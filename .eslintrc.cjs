e5f58ada2c0e5832e573efe7bdc682def280793b
HaiHai <2362349147@qq.com>
2023-11-08 18:59:08 +0800

    登录和注册完成

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,5 +33,11 @@ module.exports = {
     'vue/no-setup-props-destructure': ['off'],
     // 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
     'no-undef': 'error'
+  },
+  //eslintrc 中声明全局变量名,  解决 ElMessage 报错问题
+  globals: {
+    ElMessage: 'readonly',
+    ElMessageBox: 'readonly',
+    ElLoading: 'readonly'
   }
 }
